cce1f8ef3e89575b96d94716970a3e97bb97df95
digthedill <popssly@gmail.com>
2020-12-20 11:55:03 -0600

    json tweaks

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   // Copy the `imgs/` directory
   eleventyConfig.addPassthroughCopy("imgs");
   eleventyConfig.setFrontMatterParsingOptions({ excerpt: true });
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
   return {
     jsDataFileSuffix: ".11tydata",

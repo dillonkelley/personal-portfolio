a25a9fc5bee264b89e36cb4996a3af124a5338d3
digthedill <popssly@gmail.com>
2020-12-19 22:40:38 -0600

    fix postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ module.exports = {
   plugins: [
     require("tailwindcss"),
     require("postcss-nesting"),
+    require("postcss-preset-env")({ stage: 1 }),
     require("autoprefixer"),
+    require("postcss-clean"),
   ],
 };

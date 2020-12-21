fca43596064a48a4309ba0c9d143eb7d002dd87c
digthedill <popssly@gmail.com>
2020-12-20 19:18:27 -0600

    added css purge

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require("path");
+const glob = require("glob");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname, "src"),
+};
 
 module.exports = {
   entry: "./src/scripts/main.js",
@@ -7,7 +13,14 @@ module.exports = {
     path: path.resolve(__dirname, "_site/assets"),
     filename: "main.js",
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    }),
+  ],
   module: {
     rules: [
       {

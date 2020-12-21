610f5ccc0dd18ee03a5555d0814766103866d6c4
digthedill <popssly@gmail.com>
2020-12-20 18:57:49 -0600

    webpack tweaks

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,6 @@ module.exports = {
   plugins: [
     require("tailwindcss"),
     require("postcss-nesting"),
-    require("postcss-preset-env")({ stage: 1 }),
     require("autoprefixer"),
-    require("postcss-clean"),
   ],
 };
